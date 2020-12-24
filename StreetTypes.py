979b0cb77a0b982bbca7d680d3ad6fd5f259bd5f
ebstockdale <75113501+ebstockdale@users.noreply.github.com>
2020-12-24 08:35:20 -0800

    Add files via upload

--- StreetTypes.py
+++ StreetTypes.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[13]:
 
+
+# List of expected street types
+
+expected = ['Street', 'Avenue', 'Boulevard', 'Road', 'Place', 'Parkway', 'Lane',
+            'Drive']
+
+
+mapping = { "St": "Street",
+            "St.": "Street",
+            "street": "Street",
+            "Ave": "Avenue",
+            "Ave.": "Avenue",
+            "Blvd": "Boulevard",
+            "Blvd.": "Boulevard",
+            "Boulavard": "Boulevard",
+            "Rd": "Road",
+            "Rd.": "Road",
+            "RD": "Road",
+            "Pl": "Place",
+            "Pl.": "Place",
+            "PKWY": "Parkway",
+            "Pkwy": "Parkway",
+            "Ln": "Lane",
+            "Ln.": "Lane",
+            "Dr": "Drive",
+            "Dr.": "Drive"
+            }
+
+
+#Searches through the datafile and compares the values to the expected list above. 
+#It then prints the list of values identified for review.
 
 import xml.etree.cElementTree as ET
 import pprint
@@ -17,7 +48,7 @@ street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 expected = ['Street', 'Avenue', 'Boulevard', 'Road', 'Place', 'Parkway', 'Lane',
             'Drive']
 
-
+# Adds the street_name to the dictionary street_types if the street_name is not expected
 def audit_street_type(street_types, street_name):
     m = street_type_re.search(street_name)
     if m:
@@ -25,7 +56,7 @@ def audit_street_type(street_types, street_name):
         if street_type not in expected:
             street_types[street_type].add(street_name)
 
-
+# Returns a boolean: True if the value of the 'k' attribute is a street name
 def is_street_name(elem):
     return (elem.attrib['k'] == "addr:street")
 
@@ -44,10 +75,4 @@ def audit(osmfile):
 
 seattle_street_types = audit(datafile)
 pprint.pprint(dict(seattle_street_types))
-
-
-# In[1]:
-
-
-
 
